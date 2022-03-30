469f32a1429c7090e49059091e064d8fd331426c
Nidhip859 <nidhip859@gmail.com>
2022-03-30 18:31:27 -0400

    detail page

---
+++ resources/views/photodetail.blade.php
@@ -0,0 +1,43 @@
+@if (session()->has('user'))
+@include('header1')
+
+@else
+@include('header')
+
+@endif
+
+<form>
+<div class="container mt-5 mb-5">
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="images p-3">
+                            <div class="text-center p-4"> <img id="main-image" src="{{url('storage/'.$item['filepath'])}}" width="250" /> </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="product p-4">
+                            <a class="d-flex justify-content-between align-items-center" href="{{url('/')}}">
+                                <div class="d-flex align-items-center"> <i class="fa fa-long-arrow-left"></i> <span class="ml-1">Back</span> </div>
+</a>
+                            <div class="mt-4 mb-3"> <span class="text-uppercase text-muted brand"></span>
+                                <h5 class="text-uppercase">Galaxy</h5>
+                                <div class="price d-flex flex-row align-items-center"> <span class="act-price b">${{$item['price']}}</span>
+                                    <div class="ml-2"> <small class="dis-price"></small> <span></span> </div>
+                                </div>
+                            </div>
+                            <p class="about">{{$item['description']}}</p>
+                            <div class="cart mt-4 align-items-center"> <a class="btn btn-danger text-uppercase mr-2 px-4"href="{{url('cart')}}/{{$item['photo_id']}}">Add to cart</a><a  class="btn btn-warning text-uppercase mr-2 px-4" href="{{url('/payment')}}"> Buy Now</a> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</form>
+
+
+@include('footer')

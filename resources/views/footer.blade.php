5bef5d23812437508bc5d8f4ac0185934cfba0af
kevalpatel1881 <patel.keval304@gmail.com>
2022-03-31 12:52:25 -0400

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,11 +1,32 @@
-
-<footer class="dark mt-100 fixed-bottom">
+<footer class="text-center mt-100 p-5 bg-black text-white fixed-bottom" style="opacity: 0.8;" id="footerBackground">
+    <!-- <a href="#" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a> -->
+    <div class="w3-xlarge w3-section">
+        <a href="https://www.facebook.com/"><i onmouseover="this.style.opacity = '0.5'" onmouseout="this.style.opacity = '1'" class="fa fa-facebook-official fa-lg text-light"></i></a>
+        <a href="https://www.instagram.com/"><i onmouseover="this.style.opacity = '0.5'" onmouseout="this.style.opacity = '1'" class="fa fa-instagram fa-lg text-light"></i></a>
+        <a href="https://www.twitter.com/"><i onmouseover="this.style.opacity = '0.5'" onmouseout="this.style.opacity = '1'" class="fa fa-twitter fa-lg text-light"></i></a>
+        <a href="https://www.linkedin.com/"><i onmouseover="this.style.opacity = '0.5'" onmouseout="this.style.opacity = '1'" class="fa fa-linkedin fa-lg text-light"></i></a>
+    </div>
+    <p class="h5">Copyright &copy; 2022 SnapCart</p>
+    <p class="h6">All rights reserved.</p>
+</footer>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- <footer class="dark mt-100 fixed-bottom">
 
 <div class="footer-copyright text-center text-white py-3">© 2022 Copyright: Snapcart.com</div>
-</footer>
+</footer> -->
+<script>
+    document.getElementById("footerBackground").onmouseover = function() {
+        this.style.opacity = 1;
+    };
+    document.getElementById("footerBackground").onmouseout = function() {
+        this.style.opacity = 0.8;
+    };
+</script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-
 </body>
+
 </html>

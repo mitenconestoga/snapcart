51bf9ae42cfa2b6b2e907597d503a026a51b3360
kevalpatel1881 <patel.keval304@gmail.com>
2022-04-05 12:53:32 -0400

    footer changes

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,16 +1,16 @@
-
-<footer class="dark mt-100 fixed-bottom">
-<footer class="page-footer font-small dark mt-100">
-
-
-<div class="footer-copyright text-center text-white py-3">© 2022 Copyright: Snapcart.com</div>
-</footer> -->
+<!-- <footer class="dark mt-100 fixed-bottom"> -->
+<footer class="page-footer font-small bg-dark" id="footerBackground">
+    <div class="footer-copyright text-center text-white py-3">
+        <p class="mt-3">Copyright © 2022 SnapCart</p>
+        <p>All rights reserved.</p>
+    </div>
+</footer>
 <script>
     document.getElementById("footerBackground").onmouseover = function() {
-        this.style.opacity = 1;
+        this.style.opacity = 0.8;
     };
     document.getElementById("footerBackground").onmouseout = function() {
-        this.style.opacity = 0.8;
+        this.style.opacity = 1;
     };
 </script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
